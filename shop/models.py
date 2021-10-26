926554ea56d6250e692a3c1a0aa1802bdde5314e
Yazek13 <fedorov_d_l@mail.ru>
2021-10-26 21:53:44 +1000

    create models.py

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Section(models.Model):
+    title = models.CharField(max_length=70, help_text='Введите название раздела', blank=True, unique=True,
+                             verbose_name="Название раздела")
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Раздел"
+        verbose_name_plural = "Разделы"
+
+    def __str__(self):
+        return self.title
