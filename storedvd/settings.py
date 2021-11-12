5221e9a0c5cee0569125e2d78c1a9e4390bad9ab
Fedorov Dmitriy <fedorov@infodev.ru>
2021-11-12 12:20:07 +1000

    create settinng bd postgres

--- storedvd/settings.py
+++ storedvd/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'storedvd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
